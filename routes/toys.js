6ab0cc8763892df4219e0fa02668bd0579689a0f
fvilmot <fvilmot@gmail.com>
2022-12-10 17:13:50 +0100

    Index et show toy - gère pas si catégorie est null

--- routes/toys.js
+++ routes/toys.js
@@ -1,12 +1,25 @@
-const express = require("express");
-const router = express.Router();
+function toysRoutes(app, db) {
+  // Index toys
+  app.get("/toys", async (req, res) => {
+    const responseDB =
+      await db.query(`SELECT toys.id, categories.name AS category, description, toys.name, price FROM toys
+    JOIN categories
+    ON categories.id = toys.category`);
+    res.json({ status: 200, responseDB });
+  });
 
-const toysCtrl = require("../controllers/toys");
+  // Show toy
+  app.get("/toys/:id", async (req, res) => {
+    const id = req.params.id;
+    const responseDB = await db.query(
+      `SELECT toys.id, categories.name AS category, description, toys.name, price FROM toys
+    JOIN categories
+    ON categories.id = toys.category
+    WHERE toys.id = ?`,
+      [id]
+    );
+    res.json({ status: 200, responseDB });
+  });
+}
 
-router.get("/", toysCtrl.indexToys);
-router.get("/:id", toysCtrl.getOneToy);
-router.post("/", toysCtrl.createToy);
-router.put("/:id", toysCtrl.modifyToy);
-router.delete("/:id", toysCtrl.deleteToy);
-
-module.exports = router;
+module.exports = toysRoutes;
