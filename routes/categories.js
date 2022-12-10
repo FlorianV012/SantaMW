144f313af89b922d42ae0bc6cfe5a522ffabf741
fvilmot <fvilmot@gmail.com>
2022-12-10 16:41:32 +0100

    CRUD category

--- routes/categories.js
+++ routes/categories.js
@@ -23,6 +23,26 @@ function categoriesRoutes(app, db) {
     );
     res.json({ status: 200, responseDB });
   });
+
+  // Update category
+  app.put("/categories/:id", async (req, res) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const responseDB = await db.query(
+      "UPDATE categories SET name = ? WHERE id = ?",
+      [name, id]
+    );
+    res.json({ status: 200, responseDB });
+  });
+
+  // Delete category
+  app.delete("/categories/:id", async (req, res) => {
+    const id = req.params.id;
+    const responseDB = await db.query(`DELETE FROM categories WHERE id = ?`, [
+      id,
+    ]);
+    res.json({ status: 200, responseDB });
+  });
 }
 
 module.exports = categoriesRoutes;
